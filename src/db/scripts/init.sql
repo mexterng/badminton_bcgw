b94ff3bc1929f332d861689a05bd1c8e16d14d89
Paul Meyer <okovessimon@yahoo.de>
2025-08-07 14:54:17 +0200

    refactor(databse): update definitions

--- src/db/scripts/init.sql
+++ src/db/scripts/init.sql
@@ -1,21 +1,24 @@
+CREATE DATABASE IF NOT EXISTS scu_db;
+USE scu_db;
+
 -- game logic
-CREATE TABLE age_division (
+CREATE TABLE IF NOT EXISTS age_division (
     age_division_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(255)
+    description VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE member (
+CREATE TABLE IF NOT EXISTS member (
     member_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    display_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    display_name VARCHAR(50) NOT NULL UNIQUE,
     gender ENUM('m', 'w', 'd'),
     age_division_id INTEGER NOT NULL,
-    overall_active BOOLEAN NOT NULL,
+    overall_active BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (age_division_id) REFERENCES age_division(age_division_id)
 );
 
-CREATE TABLE doubles (
+CREATE TABLE IF NOT EXISTS doubles (
     doubles_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     player_a INTEGER NOT NULL,
     player_b INTEGER NOT NULL,
@@ -23,22 +26,22 @@ CREATE TABLE doubles (
     FOREIGN KEY (player_b) REFERENCES member(member_id)
 );
 
-CREATE TABLE game_sets (
+CREATE TABLE IF NOT EXISTS game_sets (
     set_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     points_a INTEGER NOT NULL,
     points_b INTEGER NOT NULL
 );
 
-CREATE TABLE games_single (
+CREATE TABLE IF NOT EXISTS games_single (
     game_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     player_a INTEGER NOT NULL,
     player_b INTEGER NOT NULL,
-    timestamp DATETIME NOT NULL,
+    timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     set_one INTEGER,
     set_two INTEGER,
     set_three INTEGER,
     winner_id INTEGER NOT NULL,
-    valid BOOLEAN NOT NULL,
+    valid BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (player_a) REFERENCES member(member_id),
     FOREIGN KEY (player_b) REFERENCES member(member_id),
     FOREIGN KEY (set_one) REFERENCES game_sets(set_id),
@@ -47,16 +50,16 @@ CREATE TABLE games_single (
     FOREIGN KEY (winner_id) REFERENCES member(member_id)
 );
 
-CREATE TABLE games_double (
-    game_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS games_double (
+    game_id INTEGER NOT NULL PRIMARY KEY,
     doubles_id_a INTEGER NOT NULL,
     doubles_id_b INTEGER NOT NULL,
-    timestamp DATETIME NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     set_one INTEGER,
     set_two INTEGER,
     set_three INTEGER,
     winner_id INTEGER NOT NULL,
-    valid BOOLEAN NOT NULL,
+    valid BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (doubles_id_a) REFERENCES doubles(doubles_id),
     FOREIGN KEY (doubles_id_b) REFERENCES doubles(doubles_id),
     FOREIGN KEY (set_one) REFERENCES game_sets(set_id),
@@ -66,23 +69,23 @@ CREATE TABLE games_double (
 );
 
 -- user management
-CREATE TABLE roles (
+CREATE TABLE IF NOT EXISTS roles (
     role_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(255) UNIQUE NOT NULL
+    description VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    username VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(50) UNIQUE NOT NULL,
     role_id INTEGER NOT NULL,
-    tag VARCHAR(255),
+    tag VARCHAR(50),
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
-CREATE TABLE user_credentials (
+CREATE TABLE IF NOT EXISTS user_credentials (
     user_id INTEGER PRIMARY KEY,
-    credential VARCHAR(255) NOT NULL,
+    credential VARCHAR(50) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
