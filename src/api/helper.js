7c05d480603510e20b964dbaac86d75083f9b1c4
Paul Meyer <okovessimon@yahoo.de>
2025-09-05 14:42:55 +0200

    feat(api): add functions to calculate position in pyramid and reverse

--- src/api/helper.js
+++ src/api/helper.js
@@ -10,7 +10,22 @@ function emptyOrRows(rows) {
   return rows;
 }
 
+// Calculate row and column in pyramid
+function positionInPyramid(place) {
+  let row = Math.floor((Math.sqrt(1 + 8 * place) - 1) / 2);
+  if ((row * (row + 1)) / 2 < place) row += 1;
+  let col = place - (row * (row - 1)) / 2;
+  return [row, col];
+}
+
+// Map back from (row, col) to absolute position number
+function placeFromPositionInPyramid(row, col) {
+  return Math.floor((row * (row - 1)) / 2) + col;
+}
+
 module.exports = {
   getOffset,
-  emptyOrRows
+  emptyOrRows,
+  positionInPyramid,
+  placeFromPositionInPyramid
 }
