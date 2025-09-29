420446d66f2ed2efd3a8e7258df64b71261e2607
Wagner, Felix <felix.wagner@stud.hs-coburg.de>
2025-09-29 13:55:26 +0200

    feat(ui): create footer und add basic routing functionality for footer and home vnavigation in header

--- src/web/scu-webservice/src/app/create-match/create-match.component.ts
+++ src/web/scu-webservice/src/app/create-match/create-match.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../subcomponents/header/header.component';
+import { FooterComponent } from '../subcomponents/footer/footer.component';
 
 @Component({
   selector: 'app-create-match',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './create-match.component.html',
   styleUrl: './create-match.component.scss'
 })
