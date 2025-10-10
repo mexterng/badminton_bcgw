b05cc1ec82e0c218da7eaa741c2da7b8bbb89877
Paul Meyer <okovessimon@yahoo.de>
2025-10-10 11:29:38 +0200

    feat(ui): game-category-selector

--- src/web/scu-webservice/src/app/subcomponents/header/header.component.ts
+++ src/web/scu-webservice/src/app/subcomponents/header/header.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
