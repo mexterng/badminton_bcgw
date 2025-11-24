cae5d8eace207d9bb5eddb630e2dc271948b5e35
Paul Meyer <okovessimon@yahoo.de>
2025-11-24 13:25:18 +0100

    refactor(app): clean up unused component imports

--- src/web/scu-webservice/src/app/app.component.ts
+++ src/web/scu-webservice/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './subcomponents/header/header.component';
-import { FooterComponent } from './subcomponents/footer/footer.component';
-import { PyramideComponent } from './pyramide/pyramide.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-  RouterOutlet,
-  HeaderComponent,
-  FooterComponent,
-  PyramideComponent
+  RouterOutlet
 ]
 ,
   templateUrl: './app.component.html',
