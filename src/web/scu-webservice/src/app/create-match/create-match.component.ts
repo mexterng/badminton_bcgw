d9cac6ec37e16812614d956a1e073137abb5165e
Paul Meyer <okovessimon@yahoo.de>
2025-11-25 12:17:16 +0100

    feat(ui): add game-category-selector

--- src/web/scu-webservice/src/app/create-match/create-match.component.ts
+++ src/web/scu-webservice/src/app/create-match/create-match.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from '../subcomponents/header/header.component';
+import { GameCategorySelectorComponent } from '../subcomponents/game-category-selector/game-category-selector.component';
 import { FooterComponent } from '../subcomponents/footer/footer.component';
 
 @Component({
   selector: 'app-create-match',
-  imports: [HeaderComponent, FooterComponent],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, HeaderComponent, FooterComponent, GameCategorySelectorComponent],
   templateUrl: './create-match.component.html',
   styleUrl: './create-match.component.scss'
 })
 export class CreateMatchComponent {
   title="Neues Spiel";
+  data: any[] = [];
+  loading = false;
+
+  // handle selection from child component
+  onSelectionChanged(event: { ageClass: string | null; playType: string | null}) {
+    if (!event.ageClass || !event.playType) return;
+  }
 }
