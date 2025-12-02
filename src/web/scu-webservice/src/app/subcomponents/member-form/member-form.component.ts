5ff8367e349cef787f2248a921806f41926a9b4a
Paul Meyer <okovessimon@yahoo.de>
2025-12-02 12:22:52 +0100

    feat(ui): init member-form.component

---
+++ src/web/scu-webservice/src/app/subcomponents/member-form/member-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-member-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-form.component.html',
+  styleUrl: './member-form.component.scss'
+})
+export class MemberFormComponent {
+}
