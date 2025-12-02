8c38bbefc38b01c6843b2d88a1b1f1901ca3770c
Paul Meyer <okovessimon@yahoo.de>
2025-12-02 13:58:05 +0100

    feat(ui): load agegroup data from db (AgeDivisionService)

--- src/web/scu-webservice/src/app/subcomponents/member-form/member-form.component.ts
+++ src/web/scu-webservice/src/app/subcomponents/member-form/member-form.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { AgeDivisionService } from '../../services/age-division.service';
+
+interface AgeDivision {
+  age_division_id: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-member-form',
@@ -23,16 +30,25 @@ export class MemberFormComponent {
   });
 
   genders = [
-    { value: 'm', label: 'männlich' },
-    { value: 'w', label: 'weiblich' },
+    { value: 'm', label: 'Männlich' },
+    { value: 'w', label: 'Weiblich' },
   ];
 
-  ageGroups = [
-    { value: 'u10', label: 'U10' },
-    { value: 'u12', label: 'U12' },
-    { value: 'u14', label: 'U14' },
-    { value: 'u16', label: 'U16' },
-  ];
+  agegroups: { value: string; label: string }[] = [];
+
+  constructor(private ageService: AgeDivisionService) {}
+
+  async ngOnInit() {
+    await this.loadAgeGroups();
+  }
+
+  private async loadAgeGroups() {
+    const allDivisions: AgeDivision[] = await this.ageService.getAgeDivisions();
+    this.agegroups = allDivisions.map(d => ({
+      value: d.age_division_id,
+      label: d.description
+    }));
+  }
 
   onSave(): void {
     console.log('Benutzer speichern:', this.memberForm.value);
