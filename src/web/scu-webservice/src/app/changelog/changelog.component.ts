cc3657c7bfe02396b69eef2066405fdff6776624
Paul Meyer <okovessimon@yahoo.de>
2025-12-15 02:40:05 +0100

    feat(ui): add changelog component

---
+++ src/web/scu-webservice/src/app/changelog/changelog.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { marked } from 'marked';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { HeaderComponent } from '../subcomponents/header/header.component';
+
+@Component({
+  selector: 'app-changelog',
+  imports: [HeaderComponent],
+  templateUrl: './changelog.component.html',
+  styleUrls: ['./changelog.component.scss']
+})
+export class ChangelogComponent implements OnInit {
+  title = 'Changelog';
+  html: SafeHtml = '';
+
+  constructor(
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  async ngOnInit() {
+    try {
+      const md = await firstValueFrom(
+        this.http.get('/CHANGELOG.md', { responseType: 'text' })
+      );
+
+      const rawHtml = await marked.parse(md);
+
+      this.html = this.sanitizer.bypassSecurityTrustHtml(rawHtml);
+
+    } catch (err) {
+      console.error('Fehler beim Laden des Changelogs:', err);
+      this.html = '<p>Changelog konnte nicht geladen werden.</p>';
+    }
+  }
+}
