ac26c912ecf57a12a27896956c218498962c082b
Paul Meyer <okovessimon@yahoo.de>
2026-07-14 12:03:22 +0200

    feat(ui): style login state on home

--- src/web/webservice/src/app/home/home.component.ts
+++ src/web/webservice/src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from '../subcomponents/header/header.component';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
